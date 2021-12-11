31bc348844e0af92034b539717dca2bc8c7b5fcf
Csaba Obsitos <obsitoscs@gmail.com>
2021-12-11 21:18:51 +0100

    Módosításon kívül minden működik + kommentelés
    A főbb dolgok kilettek kommentelve, és működik, futtatható a program. Egy hiba: A kipipált listaelemek közül csak az utolsók változtatnak színt, mivel az a listaelem, erre egy megoldást kell majd találni.

--- ListApp/MainWindow.xaml.cs
+++ ListApp/MainWindow.xaml.cs
@@ -22,43 +22,79 @@ namespace ListApp
     public partial class MainWindow : Window
     {
         List<string> Elemek = new List<string>();
+        List<string> TElemek = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
-            if (File.Exists(@"Elemek")){
-                foreach (string line in File.ReadAllLines(@"Elemek")){
+
+            //Mentett lista betöltése
+            if (File.Exists(@"Elemek")) {
+                foreach (string line in File.ReadAllLines(@"Elemek")) {
                     Elemek.Add(line);
                     CheckBox listaelem = new CheckBox();
                     listaelem.Content = line;
                     Lista.Items.Add(listaelem);
                 }
             }
+
+            //Mentett Törölt Lista betöltése
+            if (File.Exists(@"TElemek"))
+            {
+                foreach (string line in File.ReadAllLines(@"TElemek"))
+                {
+                    TElemek.Add(line);
+                    CheckBox Tlistaelem = new CheckBox();
+                    Tlistaelem.Content = line;
+                    TLista.Items.Add(Tlistaelem);
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            //Listaelem felvétele
             File.AppendAllText(@"Elemek", Adat.Text + System.Environment.NewLine);
             Elemek.Add(Adat.Text);
             CheckBox listaelem = new CheckBox();
             listaelem.Content = Adat.Text;
+            listaelem.AddHandler(CheckBox.CheckedEvent, new RoutedEventHandler(listaelem_isChecked));
             Lista.Items.Add(listaelem);
+        }
+
+        private void listaelem_isChecked(object sender, RoutedEventArgs e)
+        {
+            (sender as CheckBox).Foreground = new SolidColorBrush(Colors.Gray);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Lista.Items.Remove(Adat.Text);
+            //Áthelyezés a kukába
             File.AppendAllText(@"TElemek", Adat.Text + System.Environment.NewLine);
-            Elemek.Remove(Adat.Text);
+            TElemek.Add(Adat.Text);
             CheckBox Tlistaelem = new CheckBox();
             Tlistaelem.Content = Adat.Text;
             TLista.Items.Add(Tlistaelem);
+            Elemek.RemoveAt(Lista.SelectedIndex);
+            Lista.Items.RemoveAt(Lista.SelectedIndex);
+            Lista.Items.Refresh();
         }
 
         private void Lista_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Adat.Text = (Lista.SelectedItem as ListBoxItem).Content.ToString();
+            //A  beviteli adatmezőnél a kiválasztott elem nevét helyettesíti be
+            if (Lista.SelectedIndex < Elemek.Count && Lista.SelectedIndex > -1)
+            {
+                Adat.Text = Elemek[Lista.SelectedIndex];
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //Kukába helyezett elemek törlése
+            TElemek.Clear();
+            TLista.Items.Clear();
+            File.Delete(@"TElemek");
         }
     }
 }
