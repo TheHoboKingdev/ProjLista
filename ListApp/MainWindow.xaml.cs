f9b081c86105d72aed2d94bcf577439f8758e8ca
Csaba Obsitos <obsitoscs@gmail.com>
2021-12-11 15:30:47 +0100

    Hozzáadás + mentés/betöltés + Kukába helyezés
    Nem működik, de a hozzáadás, a kukába helyezés, és a betöltés teljesen működik.

--- ListApp/MainWindow.xaml.cs
+++ ListApp/MainWindow.xaml.cs
@@ -29,6 +29,9 @@ namespace ListApp
             if (File.Exists(@"Elemek")){
                 foreach (string line in File.ReadAllLines(@"Elemek")){
                     Elemek.Add(line);
+                    CheckBox listaelem = new CheckBox();
+                    listaelem.Content = line;
+                    Lista.Items.Add(listaelem);
                 }
             }
         }
@@ -36,9 +39,26 @@ namespace ListApp
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            File.AppendAllText(@"Elemek", Adat.Text + "/n");
+            File.AppendAllText(@"Elemek", Adat.Text + System.Environment.NewLine);
             Elemek.Add(Adat.Text);
-            Lista.ItemsSource = Elemek;
+            CheckBox listaelem = new CheckBox();
+            listaelem.Content = Adat.Text;
+            Lista.Items.Add(listaelem);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Lista.Items.Remove(Adat.Text);
+            File.AppendAllText(@"TElemek", Adat.Text + System.Environment.NewLine);
+            Elemek.Remove(Adat.Text);
+            CheckBox Tlistaelem = new CheckBox();
+            Tlistaelem.Content = Adat.Text;
+            TLista.Items.Add(Tlistaelem);
+        }
+
+        private void Lista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Adat.Text = (Lista.SelectedItem as ListBoxItem).Content.ToString();
         }
     }
 }
